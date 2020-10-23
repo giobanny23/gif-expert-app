159810b2fb16b5150076fd117fa09f0cc213e99f
giobanny23 <edgar_2_62@hotmail.com>
2020-10-22 19:45:45 -0500

    Poryecto Reactjs

---
+++ src/GifExpertApp.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+export const GifExpertApp =() =>{
+
+    //const categories = ['One Punch','SAmurai x', 'Dragon Ball'];
+    
+   const [categories, setcategories] = useState(['One Punch','SAmurai x', 'Dragon Ball']);
+
+   const handleAdd = () =>{
+       //setCategories ([ 'HunterXHunter'...categories]);
+       setcategories(cats => [...cats, 'HunterXHunter']);
+   } 
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr/>  
+            <button onClick={handleAdd}>Agregar</button>
+            <ol>
+            {
+                categories.map( category => {
+                return <li key ={category}>{ category }</li>
+                })
+            } 
+            </ol>        
+        </>
+    )    
+}

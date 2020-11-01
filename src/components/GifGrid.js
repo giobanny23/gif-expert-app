6f73f539d1f4e7724815ea1bd22c8d9c2e543cc1
giobanny23 <edgar_2_62@hotmail.com>
2020-10-31 21:22:01 -0600

    Custom Hooks

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,50 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import { GifGridItem } from './GifGridItem';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFectchGifs'
+//import { getGifs } from '../helpers/getGifs';
+//import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
-    const [images, setImages] = useState([]);
+    // const [images, setImages] = useState([]);
 
-    useEffect( () =>{
-        getGifs();
-    }, [])
+    const { loading }= useFetchGifs();
 
-   const getGifs = async() => {  
+    // console.log(data);
+    // console.log(loading);
 
-    const url ='https://api.giphy.com/v1/gifs/search?q=rick+and+morty&limit=10&api_key=Zxnme4AmQGcmkgr7lTTO7tzn5gxjZMi2'
-    const resp = await fetch(url);
-    const { data } = await resp.json();
+    // useEffect( () =>{
+    //     getGifs( category)
+    //     .then(setImages);
+    // }, [ category ])
 
-    const gifs = data.map(img => {
-        return{
-            id:img.id,
-            title:img.title,
-            url: img.images?.downsized_medium.url
-        }
-
-    })
-    console.log(gifs);
-    setImages( gifs);
-
-   }
-
-
+   
 
 
     return (
-        <div>
+
+        <>
             <h3>{ category }</h3> 
-           
-                {
-                    images.map( (img) =>(
-                    <GifGridItem 
-                        key= { img.id }
-                        { ...img}
-                    />
-                    ))
-                }     
-                 
-               
-        </div>
+            
+            { loading ? 'Cargando...': 'Data cargada'}
+
+            {/* <div className="card-grid">
+                
+                    {
+                        images.map( (img) =>(
+                        <GifGridItem 
+                            key= { img.id }
+                            { ...img}
+                        />
+                        ))
+                    }     
+                    
+                
+            </div> */}
+        </>
     )
 }
+
+

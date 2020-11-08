f2c2515b1861ac45aef50c1f9060805269cd1e69
giobanny23 <edgar_2_62@hotmail.com>
2020-11-07 23:02:32 -0600

    docs agregado

---
+++ docs/precache-manifest.d5ae395ed4556e2af49ab8e17c03e5f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a296de75f70fc52578fe95a81452d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a0475dc8778e30fe9b6",
+    "url": "/static/css/main.91f9237c.chunk.css"
+  },
+  {
+    "revision": "a13e31d6699d5138f112",
+    "url": "/static/js/2.bed60b36.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.bed60b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0475dc8778e30fe9b6",
+    "url": "/static/js/main.60002208.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
